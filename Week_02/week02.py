cf4a2ef45e54bb38c95f25eda41e177baf7ffaa4
charconst <44410850+charconst@users.noreply.github.com>
2020-12-12 21:37:52 -0800

    Added week02 problems

---
+++ Week_02/week02.py
@@ -0,0 +1,17 @@
+# 1. Multiply
+# Write a function multiply which multiplies x, y and returns the result
+
+# 2. Print Numbers
+# Write a function that prints out the result of adding 1..n numbers
+# e.x. n = 3
+# Output:
+# 1
+# 3
+# 6
+
+# 3. Password Length check
+# Write a function that asks the user to input a password
+# If the password has fewer than 8 characters, print a rejection
+# Otherwise, print a success message
+# How would you also check the password strength? E.g. # of numbers, chars
+# and special chars?
