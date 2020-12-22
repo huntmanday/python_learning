98b52216505854cad2758a10c44552f74d1aa30f
hday <huntman_day@yahoo.com>
2020-12-22 08:41:04 -0600

    submit question 1 homework

---
+++ Week_01/question1.py
@@ -0,0 +1,8 @@
+# 1. Write a Python script that initializes two variables
+# multiplies the result, store the result in a third variable,
+# and prints the result
+x = 2
+y = 3
+result = x + y
+print(result)
+assert(result == 5)
