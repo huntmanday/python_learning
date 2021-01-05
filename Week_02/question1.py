f830bdd1ba34f8b0701880c14e04a6098ae6a783
hday <huntman_day@yahoo.com>
2021-01-05 14:29:22 -0600

    Submitting Week02 - Question 1 homework

---
+++ Week_02/question1.py
@@ -0,0 +1,11 @@
+def multiply(x,y):
+    factor = x * y
+    print(factor)
+
+multiply(10 , 100)
+
+#Confirmed when ran = 
+
+#hday@hday-itx:~/python_learning$ /usr/bin/python3 /home/hday/python_learning/Week_02/question1.py
+#1000
+#hday@hday-itx:~/python_learning$
